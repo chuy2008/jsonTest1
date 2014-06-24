ebf924434638c62e0d05d36eec870477f4b5fd58
chuy2008 <chuylozano@ieee.org>
2014-06-24 09:55:14 -0500

    first commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,62 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import play.api.libs.json.Writes._
+
+object Application extends Controller 
+{
+  
+  def index = Action 
+  {
+    Ok(views.html.index("Your new application is ready."))
+  }
+  
+/*
+  def getCust = Action 
+  {
+      val json = inTransaction 
+      {
+         val custs = from(AppDB.custTable)(custTable => select(custTable))
+         implicit object custFormat extends Format[Customer] 
+         {
+             def reads(json: JsValue) = JsSuccess(Customer((json \ "name").as[String]))
+                                       //  (json \ "id").as[Long],
+                                       //  (json \ "name").as[Option[String]]  )
+
+             def writes(custt: Customer) = JsObject(Seq("id" -> JsNumber(custt.id),
+                                                      "name" -> Json.toJson(custt.name) ) )
+         }
+         Json.toJson(custs)
+       }
+       Ok(json).as(JSON)
+  }
+ *
+ *  
+ */  
+ 
+  def receiveCode = Action
+  {
+//    val json = generate(Map("name" -> "lll"))   
+//    Ok(json).as(JSON)
+     Ok(Json.toJson(Map("name" -> "lll", "now" -> "4:00 pm")))
+    
+  }
+  
+}
+
+/*  
+  def sayHello = Action(parse.json) { request =>
+  (request.body \ "name").asOpt[String].map { name =>
+    Ok(toJson(
+      Map("status" -> "OK", "message" -> ("Hello " + name))
+    ))
+  }.getOrElse {
+    BadRequest(toJson(
+      Map("status" -> "KO", "message" -> "Missing parameter [name]")
+    ))
+  }
+}
+*/  
